d12f95644b61101f7c9b8e1531fe0fd4a4818e39
RobertBorghese <rdborghese@gmail.com>
2018-05-09 00:10:16 -0400

    Add files via upload

---
+++ SRD_ElementalSkillColors.js
@@ -0,0 +1,248 @@
+/*:
+ * @plugindesc A remake of my original "ElementTextColors" using purely C++!
+ * @author Robert Borghese (SumRndmDde)
+ * 
+ * @param ---Affect Toggles---
+ * @desc
+ * @default
+ *
+ * @param Affect Skills?
+ * @parent ---Affect Toggles---
+ * @type boolean
+ * @desc Set to 'yes' or 'no' depending on whether you
+ * want this plugin to affect the text of Skills.
+ * @default true
+ * 
+ * @param Affect Items?
+ * @parent ---Affect Toggles---
+ * @type boolean
+ * @desc Set to 'yes' or 'no' depending on whether you
+ * want this plugin to affect the text of Items.
+ * @default true
+ * 
+ * @param ---Element IDs---
+ * @desc
+ * @default
+ *
+ * @param Element ID 1
+ * @parent ---Element IDs---
+ * @desc Sets the color for Skills/Items with damage Element ID of 1.
+ * @default 0
+ *
+ * @param Element ID 2
+ * @parent ---Element IDs---
+ * @desc Sets the color for Skills/Items with damage Element ID of 2.
+ * @default 0
+ *
+ * @param Element ID 3
+ * @parent ---Element IDs---
+ * @desc Sets the color for Skills/Items with damage Element ID of 3.
+ * @default 0
+ *
+ * @param Element ID 4
+ * @parent ---Element IDs---
+ * @desc Sets the color for Skills/Items with damage Element ID of 4.
+ * @default 0
+ *
+ * @param Element ID 5
+ * @parent ---Element IDs---
+ * @desc Sets the color for Skills/Items with damage Element ID of 5.
+ * @default 0
+ *
+ * @param Element ID 6
+ * @parent ---Element IDs---
+ * @desc Sets the color for Skills/Items with damage Element ID of 6.
+ * @default 0
+ *
+ * @param Element ID 7
+ * @parent ---Element IDs---
+ * @desc Sets the color for Skills/Items with damage Element ID of 7.
+ * @default 0
+ *
+ * @param Element ID 8
+ * @parent ---Element IDs---
+ * @desc Sets the color for Skills/Items with damage Element ID of 8.
+ * @default 0
+ *
+ * @param Element ID 9
+ * @parent ---Element IDs---
+ * @desc Sets the color for Skills/Items with damage Element ID of 9.
+ * @default 0
+ *
+ * @param Element ID 10
+ * @parent ---Element IDs---
+ * @desc Sets the color for Skills/Items with damage Element ID of 10.
+ * @default 0
+ *
+ * @param Element ID 11
+ * @parent ---Element IDs---
+ * @desc Sets the color for Skills/Items with damage Element ID of 11.
+ * @default 0
+ *
+ * @param Element ID 12
+ * @parent ---Element IDs---
+ * @desc Sets the color for Skills/Items with damage Element ID of 12.
+ * @default 0
+ *
+ * @param Element ID 13
+ * @parent ---Element IDs---
+ * @desc Sets the color for Skills/Items with damage Element ID of 13.
+ * @default 0
+ *
+ * @param Element ID 14
+ * @parent ---Element IDs---
+ * @desc Sets the color for Skills/Items with damage Element ID of 14.
+ * @default 0
+ *
+ * @param Element ID 15
+ * @parent ---Element IDs---
+ * @desc Sets the color for Skills/Items with damage Element ID of 15.
+ * @default 0
+ *
+ * @param Element ID 16
+ * @parent ---Element IDs---
+ * @desc Sets the color for Skills/Items with damage Element ID of 16.
+ * @default 0
+ *
+ * @param Element ID 17
+ * @parent ---Element IDs---
+ * @desc Sets the color for Skills/Items with damage Element ID of 17.
+ * @default 0
+ *
+ * @param Element ID 18
+ * @parent ---Element IDs---
+ * @desc Sets the color for Skills/Items with damage Element ID of 18.
+ * @default 0
+ *
+ * @param Element ID 19
+ * @parent ---Element IDs---
+ * @desc Sets the color for Skills/Items with damage Element ID of 19.
+ * @default 0
+ *
+ * @param Element ID 20
+ * @parent ---Element IDs---
+ * @desc Sets the color for Skills/Items with damage Element ID of 20.
+ * @default 0
+ *
+ * @param Element ID 21
+ * @parent ---Element IDs---
+ * @desc Sets the color for Skills/Items with damage Element ID of 21.
+ * @default 0
+ *
+ * @param Element ID 22
+ * @parent ---Element IDs---
+ * @desc Sets the color for Skills/Items with damage Element ID of 22.
+ * @default 0
+ *
+ * @param Element ID 23
+ * @parent ---Element IDs---
+ * @desc Sets the color for Skills/Items with damage Element ID of 23.
+ * @default 0
+ *
+ * @param Element ID 24
+ * @parent ---Element IDs---
+ * @desc Sets the color for Skills/Items with damage Element ID of 24.
+ * @default 0
+ *
+ * @param Element ID 25
+ * @parent ---Element IDs---
+ * @desc Sets the color for Skills/Items with damage Element ID of 25.
+ * @default 0
+ *
+ * @param Element ID 26
+ * @parent ---Element IDs---
+ * @desc Sets the color for Skills/Items with damage Element ID of 26.
+ * @default 0
+ *
+ * @param Element ID 27
+ * @parent ---Element IDs---
+ * @desc Sets the color for Skills/Items with damage Element ID of 27.
+ * @default 0
+ *
+ * @param Element ID 28
+ * @parent ---Element IDs---
+ * @desc Sets the color for Skills/Items with damage Element ID of 28.
+ * @default 0
+ *
+ * @param Element ID 29
+ * @parent ---Element IDs---
+ * @desc Sets the color for Skills/Items with damage Element ID of 29.
+ * @default 0
+ *
+ * @param Element ID 30
+ * @parent ---Element IDs---
+ * @desc Sets the color for Skills/Items with damage Element ID of 30.
+ * @default 0
+ *
+ * @help
+ *
+ * Elemental Skill Colors
+ * v1.0.0
+ * SumRndmDde
+ *
+ * =========================================================================
+ * Window.png
+ * =========================================================================
+ *
+ * For this plugin, you set the color for the text by inputing the color 
+ * number from your "Window.png" file from the System folder. By default
+ * the colors are:
+ *
+ * 0 = White
+ * 1 = Light Blue
+ * 2 = Light Red
+ * 3 = Light Green
+ * 4 = Blue-ish White
+ * 5 = Light Gray
+ * 6 = Light Yello
+ * 7 = Gray
+ * 8 = Slightly-light Gray
+ * 9 = Blue
+ * 10 = Red
+ * 11 = Green
+ * 12 = Lighter Blue
+ * 13 = Lighter Gray
+ * 14 = Yellow
+ * 15 = Black
+ * 16 = Foggy Blue
+ * 17 = Slightly-light Yellow
+ * 18 = Dark Red
+ * 19 = Dark Blue
+ * 20 = Brown-ish Orange
+ * 21 = Orange-ish Yellow
+ * 22 = Baby Blue
+ * 23 = Sky Blue
+ * 24 = Teal Green
+ * 25 = Black-ish Pink
+ * 26 = Cloudy Blue
+ * 27 = Pink
+ * 28 = Forest Green
+ * 29 = Sea Green
+ * 30 = Purple
+ * 31 = Light Purple
+ *
+ * =========================================================================
+ * Parameters
+ * =========================================================================
+ *
+ *   Element ID #
+ *
+ * Sets the color of text of Skills or Items that do elemental
+ * damage based off of the element they use.
+ *
+ * =========================================================================
+ * End of Plugin
+ * =========================================================================
+ *
+ * This is symbolic of reality.
+ * However, don't be sad.
+ * Days improve.
+ * Thanks.
+ *
+ */
+
+if(Utils.isNwjs()) {
+	console.log(require("./js/native-plugins/SRD_ElementalSkillColors").applyPlugin());
+} else {
+	console.error("SRD_ElementalSkillColors.js can only be used within NodeJS enviroments (Desktop exports only).");
+}
